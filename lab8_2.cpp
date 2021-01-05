1c7354a2b20cc40e38ad07c8f0d0a0df7ebce803
jerryc137th <76035472+jerryc137th@users.noreply.github.com>
2021-01-05 17:13:45 +0700

    finished

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 //Write the function printO() here
+
+void printO(int N,int M){
+	if (N<=0||M<=0) cout<<"Invalid input";
+	else{
+		for (int i = 0; i < N; i++){
+			for(int j = 0; j < M; j++){
+				cout<<"O";
+			}
+		 cout<<"\n";
+		}
+	}	
+}
 
 int main(){
 	
@@ -15,7 +28,7 @@ int main(){
 	cout << "\n";	
 	
 	printO(0,3);
-	cout << "\n";	
+	cout << "\n";
 	
 	printO(7,-1);
 	cout << "\n";	
